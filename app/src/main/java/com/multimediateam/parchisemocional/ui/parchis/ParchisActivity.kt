88ba8eee1d9e0f7b457c3a330738b175d67a0de3
Isaac Lacoba <isaaclacoba@gmail.com>
2020-11-15 20:58:16 +0100

    Add BottomNavigationView
        - Navigate between Parchis fragment and Pie chart fragment

--- app/src/main/java/com/multimediateam/parchisemocional/ui/parchis/ParchisActivity.kt
+++ app/src/main/java/com/multimediateam/parchisemocional/ui/parchis/ParchisActivity.kt
@@ -2,12 +2,20 @@ package com.multimediateam.parchisemocional.ui.parchis
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.multimediateam.parchisemocional.R
 import com.multimediateam.parchisemocional.R.*
 import com.multimediateam.parchisemocional.ui.parchis.ParchisFragment
+import com.multimediateam.parchisemocional.ui.plots.PlotFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.parchis_activity.*
 
 @AndroidEntryPoint
 class ParchisActivity : AppCompatActivity() {
+
+    private val TAG = "ParchisActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +27,43 @@ class ParchisActivity : AppCompatActivity() {
                 )
                 .commitNow()
         }
+        initViews()
+    }
+
+    private fun initViews() {
+        bottom_navigation
+            .setOnNavigationItemSelectedListener { item-> onNavigationItemSelected(item) }
+        bottom_navigation
+            .setOnNavigationItemReselectedListener { item -> onNavigationItemReselected(item) }
+    }
+
+    fun onNavigationItemSelected(item: MenuItem): Boolean =
+        when (item.itemId) {
+            R.id.item_emotion_panel -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(
+                        id.container,
+                        ParchisFragment.newInstance()
+                    )
+                    .commitNow()
+                true
+            }
+            R.id.item_pie_chart_panel -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(
+                        id.container,
+                        PlotFragment.newInstance()
+                    )
+                    .commitNow()
+                true
+            }
+            else -> {
+                Log.d(TAG, "onNavigationItemSelected")
+                false
+            }
+        }
+
+    fun onNavigationItemReselected(item: MenuItem) {
+        Log.d(TAG, "onNavigationItemReselected")
     }
 }
